9eda722e4e1332a09cc7c447e5c7c66ee6e557a4
Mauro Carvalho <mauro.carvalho@globalhitss.com.br>
2025-10-06 20:57:08 -0300

    chore: initial config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-type Props = {
-  title: string;
-};
-
-const Component = ({ title }: Props) => {
-  return <div>Teste</div>;
-};
 export default function Home() {
   return (
     <div>
       <h2>Rocketseat</h2>
-      <Component title={1} />
     </div>
   );
 }
