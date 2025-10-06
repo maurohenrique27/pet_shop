0497856b201b97ebf40bce4c644ef32e700bbeea
Mauro Carvalho <mauro.carvalho@globalhitss.com.br>
2025-10-06 20:55:57 -0300

    testing letfhook

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,14 @@ type Props = {
   title: string;
 };
 
-const Component = ({ title }: Props) => {};
+const Component = ({ title }: Props) => {
+  return <div>Teste</div>;
+};
 export default function Home() {
   return (
     <div>
       <h2>Rocketseat</h2>
+      <Component title={1} />
     </div>
   );
 }
-
-
-
-
