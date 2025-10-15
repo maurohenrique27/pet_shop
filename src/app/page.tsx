5481e46e9d2a7edbbd87c5e85d7e00c26756ca22
Mauro Carvalho <mauro.carvalho@globalhitss.com.br>
2025-10-15 09:47:44 -0300

    feat: structure complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
-export default function Home() {
+import { PeriodSection } from "@/components/period-section";
+import { prisma } from "@/lib/prisma";
+import { APPOINTMENT_DATA, groupAppointmentByPeriod } from "@/utils";
+
+export default async function Home() {
+  const appoint = await prisma.appointment.findMany();
+  const periods = groupAppointmentByPeriod(APPOINTMENT_DATA);
+
   return (
-    <div className="bg-background-primary p-6 ">
-      <div className="flex items-center justify-between md:mb-8">
+    <div className="bg-background-primary p-6">
+      <div className="flex items-center justify-between md:m-8">
         <div>
-          <h1 className="text-title text-content-primary mb-2">Sua agenda</h1>
-          <p className="text-paragraph-small text-content-secondary ">
-            Aqui você ver todos os clientes e serviços agendados para hoje.
+          <h1 className="text-title-size text-content-primary mb-2">
+            Sua Agenda
+          </h1>
+          <p className="text-paragraph-medium-size text-content-secondary">
+            Aqui você pode ver todos os clientes e serviços agendados para hoje.
           </p>
         </div>
+      </div>
+
+      <div className="pb-24 md:pb-0">
+        {periods.map((period, index) => (
+          <PeriodSection period={period} key={index} />
+        ))}
       </div>
     </div>
   );
---
+++ src/utils/mock-data.ts
@@ -0,0 +1,34 @@
+export const APPOINTMENT_DATA = [
+  {
+    id: "1",
+    petName: "Rex",
+    description: "Consulta",
+    tutorNamer: "João",
+    phone: "1234567890",
+    scheduleAt: new Date("2025-08-17T10:00:00"),
+  },
+  {
+    id: "2",
+    petName: "Mimi",
+    tutorNamer: "Maria",
+    description: "Banho",
+    phone: "1234567890",
+    scheduleAt: new Date("2025-08-17T11:00:00"),
+  },
+  {
+    id: "3",
+    petName: "Nina",
+    tutorNamer: "Natalia",
+    description: "Consulta",
+    phone: "1234567890",
+    scheduleAt: new Date("2025-08-17T14:00:00"),
+  },
+  {
+    id: "4",
+    petName: "Nina",
+    tutorNamer: "Natalia",
+    description: "Consulta",
+    phone: "1234567890",
+    scheduleAt: new Date("2025-08-17T19:00:00"),
+  },
+];
